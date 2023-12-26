7e839d4a32643b82336337d27b9a4202ece67867
yimengchai12 <yimeng.chai.2021@scis.smu.edu.sg>
2023-12-26 22:31:20 +0800

    edited dimension

--- components/experience.tsx
+++ components/experience.tsx
@@ -50,7 +50,7 @@ const Experience = () => {
                     ? "7px solid black"
                     : "7px solid white",
               }}
-              dateClassName="exp:text-white text-gray-950"
+              dateClassName="exp:dark:text-white text-gray-950"
               iconStyle={{
                 background: theme === "light" ? "white" : "black",
                 fontSize: "1.5rem",
